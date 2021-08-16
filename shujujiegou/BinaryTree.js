2453e62a81e0b3c708eb916f06d0c00dbc485c30
--replace-all <1372594487@qq.com>
2021-08-17 03:43:52 +0800

    add chengxubianl in binaryTree

--- shujujiegou/BinaryTree.js
+++ shujujiegou/BinaryTree.js
@@ -21,7 +21,7 @@ function BinaryTree() {
             }
         }
     }
-    
+
     this.insert = function (key) {
         var newNode = new Node(key)
         if (root == null) {
@@ -30,54 +30,79 @@ function BinaryTree() {
             insertNode(root, newNode)
         }
     }
-    const zhongxuNode = function(node,callback){
-        if(node !== null){
-        zhongxuNode(node.left,callback)
-        callback(node.key)
-        zhongxuNode(node.right,callback)
-        }
-    }
-
-    const qianxuNode  = function(node,callback){
-        if(node !== null){
+    const zhongxuNode = function (node, callback) {
+        if (node !== null) {
+            zhongxuNode(node.left, callback)
             callback(node.key)
-            qianxuNode(node.left,callback)
-            qianxuNode(node.right,callback)
+            zhongxuNode(node.right, callback)
+        }
+    }
+
+    const qianxuNode = function (node, callback) {
+        if (node !== null) {
+            callback(node.key)
+            qianxuNode(node.left, callback)
+            qianxuNode(node.right, callback)
+        }
+    }
+
+    const qianxuNodeII = function (node, arr) {
+        if (node !== null) {
+            arr.push(node.key)
+            qianxuNodeII(node.left, arr)
+            qianxuNodeII(node.right, arr)
+        }
+    }
+
+    const houxuNode = function (node, arr) {
+        if (node !== null) {
+            houxuNode(node.left, arr)
+            houxuNode(node.right, arr)
+            arr.push(node.key)
+        }
+    }
+
+    const chengxuNode = function (node) {
+        const ret = [];
+        if (!node) {
+            return ret;
+        }
+        const q = [];
+        q.push(node);
+        while (q.length !== 0) {
+            const currentLevelSize = q.length;
+            ret.push([]);
+            for (let i = 1; i <= currentLevelSize; ++i) {
+                const node = q.shift();
+                ret[ret.length - 1].push(node.val);
+                if (node.left) q.push(node.left);
+                if (node.right) q.push(node.right);
             }
-    }
-
-    const qianxuNodeII = function(node,arr){
-        if(node !== null){
-            arr.push(node.key)
-            qianxuNodeII(node.left,arr)
-            qianxuNodeII(node.right,arr)
         }
+
+        return ret;
     }
 
-    const houxuNode = function(node,arr){
-        if(node !== null){
-            houxuNode(node.left,arr)
-            houxuNode(node.right,arr)
-            arr.push(node.key)
-        }
+    this.zhongxu = function (callback) {
+        zhongxuNode(root, callback)
     }
-
-    this.zhongxu = function(callback){
-        zhongxuNode(root,callback)
+    this.qianxu = function (callback) {
+        qianxuNode(root, callback)
     }
-    this.qianxu = function(callback){
-        qianxuNode(root,callback)
-    }
-    this.qianxuII = function(){
+    this.qianxuII = function () {
         let qianxuArr = []
-        qianxuNodeII(root,qianxuArr)
+        qianxuNodeII(root, qianxuArr)
         return qianxuArr
     }
-    this.houxu = function(){
+    this.houxu = function () {
         let houxuArr = []
-        houxuNode(root,houxuArr)
+        houxuNode(root, houxuArr)
         return houxuArr
     }
-
     
+    this.chengxuNode = function(){
+        chengxuNode(root)
+    }
+
+
 }
