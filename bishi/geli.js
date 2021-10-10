708d2c7d70bd77f2a5a16cc05b987641909ce8ee
--replace-all <1372594487@qq.com>
2021-10-10 22:35:55 +0800

    initial commit

--- bishi/geli.js
+++ bishi/geli.js
@@ -1,2 +1,35 @@
-let [a,b,c,d,e] = 'hello'
-console.log([a,b,c,d,e]);
+let [a, b, c, d, e] = 'hello'
+// console.log([a,b,c,d,e]);
+
+
+// var Foo = function () {
+//     getName = function () {
+//         console.log(1);
+//     }
+//     console.log(this);
+//     return this;
+// }
+
+// Foo.getName = function () {
+//     console.log(2);
+// }
+
+// Foo.prototype.getName = function () {
+//     console.log(3);
+// }
+
+var getName = function () {
+    console.log(4);
+}
+
+function getName() {
+    console.log(5);
+}
+
+getName()
+// Foo.getName()
+// Foo().getName();
+// getName();
+// new Foo().getName()
+
+
--- selfcoding.js/http.js
+++
@@ -1,8 +0,0 @@
-var http = require('http')
-http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/plain'})
-    res.send('succeed')
-    res.end()
-}).listen(3000,()=>{
-    console.log("running in 3000");
-})
